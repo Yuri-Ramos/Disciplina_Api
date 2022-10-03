0ee134b0b93022889594e90229eab098021ada5c
Yuri Ramos <yroramos@gmail.com>
2022-10-03 19:38:14 -0300

    Aula 02

---
+++ ola-mundo/src/main/java/br/org/serratec/olamundo/controller/HelloController.java
@@ -0,0 +1,27 @@
+package br.org.serratec.olamundo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HelloController {
+    
+    @GetMapping(value="/")
+    public String GetMetHodName() {
+        return "Ola Mundo VsCode";
+    }
+
+    @GetMapping(value="/resposta")
+    public String resposta() {
+        return "Oque está fazendo aqui meu caro amigo";
+    }
+
+    @GetMapping(value="/converter")
+    public String converter(@RequestParam String name) {
+        return name.toUpperCase();
+    }
+
+}
